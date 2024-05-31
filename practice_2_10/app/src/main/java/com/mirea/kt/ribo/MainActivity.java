749ab343631497adad7f9665fd2b2b79bd30523c
newrav1k <kritsky.academi@gmail.com>
2024-05-31 23:02:50 +0300

    practice 2_10 updated

--- practice_2_10/app/src/main/java/com/mirea/kt/ribo/MainActivity.java
+++ practice_2_10/app/src/main/java/com/mirea/kt/ribo/MainActivity.java
@@ -3,7 +3,6 @@ package com.mirea.kt.ribo;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
